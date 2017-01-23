3dcfa466e9751b8b0c4c5b7b08e5a7b873de87c4
Christine <cleverett1@gmail.com>
2017-01-23 16:13:39 -0500

    accessing page

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,45 @@ const model = require('../models');
 
 router.get('/wiki', function(req, res, next){
   res.render('index')
-})
+});
 
 router.get('/wiki/add', function(req, res, next){
-  res.render('addpage.html')
-})
+  res.render('addpage.html');
+});
 
 router.post('/wiki', function(req, res, next){
-   var page = model.Page.build({
+  
+  model.Page.create({
     title: req.body.title,
     content: req.body.pageContent,
+  }).then(function(instance){
+    res.json(instance);
   });
 
-  // STUDENT ASSIGNMENT:
-  // make sure we only redirect *after* our save is complete!
-  // note: `.save` returns a promise or it can take a callback.
-  res.json(page.save());
-})
+  // res.json new page instance 
+   // page.save();
+  //.then(function(){
+  //   res.json(page);
+  // });
+
+});
+
+router.get('/wiki/:urlTitle', function(req, res, next){
+  
+  model.Page.findOne({ 
+    where: { 
+      url_title: req.params.urlTitle 
+    } 
+  })
+  .then(function(foundPage){
+    res.render('wikipage.html', {
+      title: foundPage.title,
+      content: foundPage.content
+    });
+  })
+  .catch(next);
+
+
+});
 
 module.exports= router;
